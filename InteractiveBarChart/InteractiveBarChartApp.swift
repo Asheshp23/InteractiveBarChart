ed2920d2d6f4b27f2e1cca5380f21df5a2c9a863
asheshp23 <asheshp23@gmail.com>
2024-12-30 17:59:09 -0500

    Initial Commit

---
+++ InteractiveBarChart/InteractiveBarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveBarChartApp.swift
+//  InteractiveBarChart
+//
+//  Created by Ashesh Patel on 2024-12-30.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveBarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
